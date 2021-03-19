17ad7105cd1a938bf3e134696f5ca299986a96fe
Maksim Shaynyuk <maksimshaynyuk@gmail.com>
2021-03-18 22:10:23 -0700

    added  graphics page, wasm module

--- hello/graphics.c
+++ hello/graphics.c
@@ -1,3 +1,5 @@
+// limit 512mb
+
 #define WIDTH 600
 #define HEIGHT 600
 unsigned int BUFFER[WIDTH * HEIGHT];
@@ -5,12 +7,12 @@ unsigned int BUFFER[WIDTH * HEIGHT];
 void go() {
   unsigned int screen[600][600];
   unsigned int x;
-  for (x=0; x<WIDTH; x++) {
+  for (x=0; x<600; x++) {
     unsigned int y;
-    for (y=0; y<HEIGHT; y++) {
+    for (y=0; y<600; y++) {
       unsigned int color = 0xffc2614f;
       screen[x][y] = color;
-      BUFFER[WIDTH * x * y] = screen[x][y];
+      BUFFER[WIDTH * x + y] = screen[x][y];
     }
   }
 }
